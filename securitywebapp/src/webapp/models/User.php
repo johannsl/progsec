6cfa5dd6d49fae364b2c8bd0ccdd2309d4d7765a
johannsl <johannsl@stud.ntnu.no>
2015-10-19 13:00:49 +0200

    The getters and setter are now actually readable

--- securitywebapp/src/webapp/models/User.php
+++ securitywebapp/src/webapp/models/User.php
@@ -14,12 +14,12 @@ class User
     protected $email   = null;
     protected $bio     = 'Bio is empty.';
     protected $age;
-    protected $bankAccNum;
+    protected $bankAccNum = null;
     protected $isAdmin = 0;
     protected $isDoctor = 0; 
     protected $moneyReceived;
     protected $moneySpent;
-    protected $isdoctor;
+    protected $isdoctor = 0;
 
     function __construct($username, $hash, $fullname, $address, $postcode, $moneySpent, $moneyReceived)
     {
@@ -28,147 +28,134 @@ class User
         $this->fullname = $fullname;
         $this->address = $address;
         $this->postcode = $postcode;
-		$this->moneyReceived = $moneyReceived;
         $this->moneySpent = $moneySpent;
+		$this->moneyReceived = $moneyReceived;
         
     }
 
-   // getters
-    public function getUserId()
-    {
+    // Getters
+    public function getUserId() {
         return $this->userId;
     }
 
-    public function getUsername()
-    {
+    public function getUsername() {
         return $this->username;
     }
 
-    public function getHash()
-    {
+    public function getHash() {
         return $this->hash;
-    }
-
-    public function getEmail()
-    {
-        return $this->email;
-    }
-
-    public function getBio()
-    {
-        return $this->bio;
-    }
-
-    public function getAge()
-    {
-        return $this->age;
     }
 
     public function getFullname() {
         return $this->fullname;
-    }
-	
-	public function getBankAccNum()
-    {
-        return $this->bankAccNum;
-    }
-
-	// setters
-    public function setFullname($fullname) {
-        $this->fullname = $fullname;
     }
 
     public function getAddress() {
         return $this->address;
     }
 
-    public function setAddress($address) {
-        $this->address = $address;
-    }
-
     public function getPostcode() {
         return $this->postcode;
-
     }
 
-    public function setPostcode($postcode) 
-	{
-        $this->postcode = $postcode;
-		return $this;
+    public function getMoneySpent() {
+        return $this->moneySpent;
     }
 
-    public function setUserId($userId)
-    {
+    public function getMoneyReceived() {
+        return $this->moneyReceived;
+    }
+    
+    public function getHash() {
+        return $this->hash;
+    }
+
+    public function getEmail() {
+        return $this->email;
+    }
+
+    public function getBio() {
+        return $this->bio;
+    }
+
+    public function getAge() {
+        return $this->age;
+    }
+
+	public function getBankAccNum() {
+        return $this->bankAccNum;
+    }
+
+	public function isAdmin() {
+        return $this->isAdmin === '1';
+    }
+
+    public function isDoctor() {
+        return $this->isDoctor === '1';
+    }
+
+
+	// Setters
+    public function setUserId($userId) {
         $this->userId = $userId;
         return $this;
     }
 
-    public function setUsername($username)
-    {
+    public function setUsername($username) {
         $this->username = $username;
         return $this;
     }
 
-    public function setHash($hash)
-    {
+    public function setHash($hash) {
         $this->hash = $hash;
         return $this;
     }
 
-    public function setEmail($email)
-    {
-        $this->email = $email;
+    public function setFullname($fullname) {
+        $this->fullname = $fullname;
         return $this;
     }
 
-    public function setBio($bio)
-    {
+    public function setAddress($address) {
+        $this->address = $address;
+        return $this;
+    }
+
+    public function setPostcode($postcode) {
+        $this->postcode = $postcode;
+		return $this;
+    }
+
+    public function setEmail($email) {
+        $this->email = $email;
+        return $this;
+    }
+    
+    public function setBio($bio) {
         $this->bio = $bio;
         return $this;
     }
 
-    public function setAge($age)
-    {
+    public function setAge($age) {
         $this->age = $age;
         return $this;
     }
 
-    public function getMoneySpent()
-    {
-        return $this->moneySpent;
+	public function setBankAccNum($bankAccNum) 	{
+        $this->bankAccNum = $bankAccNum;
+		return $this;
     }
 
-    public function getMoneyReceived()
-    {
-        return $this->moneyReceived;
-    }
-
-    public function setIsAdmin($isAdmin)
-    {
+    public function setIsAdmin($isAdmin) {
         $this->isAdmin = $isAdmin;
         return $this;
     }
 
-    public function setIsDoctor($isDoctor)
-    {
+    public function setIsDoctor($isDoctor) {
         $this->isDoctor = $isDoctor;
         return $this;
     }
 	
-	public function setBankAccNum($bankAccNum) 
-	{
-        $this->bankAccNum = $bankAccNum;
-		return $this;
-    }
+
 	
-	public function isAdmin() 
-    {
-        return $this->isAdmin === '1';
-    }
-
-    public function isDoctor() 
-    {
-        return $this->isDoctor === '1';
-    }
-
 }
