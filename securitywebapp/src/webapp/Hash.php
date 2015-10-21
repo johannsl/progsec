c99f3bfe01eef428bb939e369f658093954666b7
benjamin <benjamin@linuxdev.local>
2015-10-21 22:38:43 +0200

    Implemented constant time string comparison to prevent timing attacks

--- securitywebapp/src/webapp/Hash.php
+++ securitywebapp/src/webapp/Hash.php
@@ -30,7 +30,32 @@ class Hash
 
     public function check($plaintext, $hash, $salt)
     {
-        return $this->make($plaintext, $salt) === $hash;
+        return $this->strcmpConstTime($this->make($plaintext, $salt),$hash);
+    }
+
+    /**
+     * This function implements a string comparison which does
+     * not terminate on a mismatch but runs always till the end
+     * of the longer strings so no timing attack is possible
+     * by measuring the execution time 
+     */
+    public function strcmpConstTime($str1, $str2) {
+        $not_null = $str1  != null && $str2 != null;
+        $mismatch = false;
+        $upperLengthBound = max(strlen($str1),strlen($str2));
+        $lowerLengthBound = min(strlen($str1),strlen($str2));
+        for($C=0; $C<$upperLengthBound; $C++) {
+            if($C<$lowerLengthBound) {
+                //both strings are defined, so we do a real comparison
+                $mismatch = !((($str1[$C] === $str2[$C])) && !$mismatch);
+            } else {
+                //one string is not defined anymore, so it has to be a mismatch, but we compare a comparison anyway
+                $tmp = $str1[$lowerLengthBound-1] === $str2[$lowerLengthBound-1];
+                $mismatch = !(false && $mismatch); //always true, so mismatch stays true in case of string length mismatch
+            }
+        }
+
+        return $not_null && !$mismatch;
     }
 
 }
